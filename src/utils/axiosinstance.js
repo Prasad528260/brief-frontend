ceb224c51e00ce9ac4abd844b7a7aa5bc7e2eddc
Prasad528260 <prasadsubhedar5460@gmail.com>
2026-01-17 10:27:47 +0530

    payment2

--- src/utils/axiosinstance.js
+++ src/utils/axiosinstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
-    baseURL: "http://localhost:5000",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    // baseURL: "http://localhost:5000",
     withCredentials: true,
     
 })
